a21e69baa9fb3a3e1e5507dae7996b831d60e35c
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-18 12:32:39 +0530

    Delete Frontend/src directory

--- Frontend/src/models/InvestmentModel.js
+++
@@ -1,9 +0,0 @@
-export default class Investment {
-    constructor(id, name, amount, date, category) {
-        this.id = id;
-        this.name = name;
-        this.amount = amount;
-        this.date = date;
-        this.category = category;
-    }
-}
